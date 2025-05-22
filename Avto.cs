fbbfa85ad0a2c04ea39e97371f1c90b297838208
Anna <asy0107@mail.ru>
2025-05-22 10:44:02 +0300

    Avto

---
+++ Avto.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+
+class Avto
+{
+    private string avto_number;
+    private float fuel_spend; // расход топлива
+    private float probeg; // пробег
+    private float petrol; // бензин в баке
+    private float obem_bac;
+    private int x, y;
+    private float speed; // скорость
+    public static Avto[] allCars = new Avto[5];
+    public static int carCount = 0;
+    private Avto()
+    {
+        avto_number = "Мяу";
+        petrol = 0;
+        fuel_spend = 0;
+        probeg = 0;
+        obem_bac = 0;
+        x = 0;
+        y = 0;
+        speed = 0;
+    }
+
+    // Полный конструктор
+    public Avto(string nom, float bak, float ras, float tank)
+    {
+        avto_number = nom;
+        petrol = (int)bak;
+        fuel_spend = ras;
+        obem_bac = tank;
+        probeg = 0;
+        x = 0;
+        y = 0;
+        speed = 0;
+    }
+
+    public void Out()
+    {
+        Console.WriteLine($"Номер авто : {avto_number}");
+        Console.WriteLine($"Количество бензина в баке : {petrol} л");
+        Console.WriteLine($"Расход бензина на 100 км : {fuel_spend} л");
+        Console.WriteLine($"Обьем бака : {obem_bac}");
+        Console.WriteLine($"Пробег : {probeg} км");
+        Console.WriteLine($"Текущая позиция : ({x}, {y})");
+        Console.WriteLine($"Текущая скорость : {speed} км/ч");
+    }
+
+    public void Zapravka(float top)
+    {
+        if (top <= 0)
+        {
+            Console.WriteLine("Нельзя заправить отрицательное количество топлива.");
+            return;
+        }
+        if (petrol + top <= obem_bac)
+        {
+            petrol += (int)top;
+            Console.WriteLine($"Заправлено {top} л. Всего: {petrol} л");
+        }
+        else
+        {
+            Console.WriteLine("Вы не можите заправить больше чем есть в баке");
+        }
+    }
+
+
+    public void Move()
+    {
+        while (true)
+        {
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("1 - Совершить поездку");
+            Console.WriteLine("2 - Выход");
+            Console.Write("Выберите действие: ");
+            Console.WriteLine();
+            int direction = Program.ReadInt();
+
+            if (direction == 2) return;
+            int newX = x, newY = y;
+            int dx;
+            int dy;
+            if (direction == 1)
+            {
+                while (true)
+                {
+                    Console.Write("Введите смещение по X: ");
+                    if ((int.TryParse(Console.ReadLine(), out dx) && dx >= 0))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Нельзя вводить отрицатеьные числа и буковки");
+                    }
+                }
+                while (true)
+                {
+                    Console.Write("Введите смещение по Y: ");
+                    if ((int.TryParse(Console.ReadLine(), out dy) && dy >= 0))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Нельзя вводить отрицатеьные числа и буковки");
+                    }
+
+                }
+                newX = x + dx;
+                newY = y + dy;
+            }
+
+            else
+            {
+                Console.WriteLine("Неверный выбор.");
+                continue;
+            }
+
+            float km = RaschetRasstoyaniya(x, y, newX, newY);
+            float neededFuel = (km / 100) * fuel_spend;
+            Console.WriteLine($"До новой точки: {km:F2} км, нужно топлива: {neededFuel} л");
+
+            Razgon();
+
+            double remainingKm = km;
+
+            while (true)
+            {
+                float fuelRange = (petrol / fuel_spend) * 100;
+
+                if (fuelRange >= remainingKm)
+                {
+                    petrol -= (float)(remainingKm / 100) * fuel_spend;
+                    probeg += km;
+                    x = newX;
+                    y = newY;
+                    Tormozhenie();
+                    Ostatok();
+                    break;
+                }
+                else
+                {
+                    remainingKm -= fuelRange;
+                    probeg += fuelRange;
+                    petrol = 0;
+                    Console.WriteLine($"Топливо закончилось. Осталось проехать: {remainingKm:F2} км");
+                    Console.WriteLine($"Нужно долить: {(remainingKm * fuel_spend / 100)} л");
+                    Console.WriteLine("1 - Заправиться");
+                    Console.WriteLine("2 - Отменить поездку");
+                    Console.Write("Ваш выбор: ");
+                    Console.WriteLine();
+
+                    int choice = Program.ReadInt();
+
+                    if (choice == 1)
+                    {
+                        Console.Write("Введите количество топлива: ");
+                        float fuelToAdd = Program.ReadFloat();
+                        if (fuelToAdd > 0)
+                        {
+                            Zapravka(fuelToAdd);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ошибка: количество должно быть положительным.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Поездка отменена");
+                        Tormozhenie();
+                        break;
+                    }
+                }
+            }
+
+            Console.Write("Продолжить движение? (да/нет): ");
+            if (Console.ReadLine().ToLower() != "да")
+            {
+                break;
+            }
+        }
+    }
+
+    public void Razgon()
+    {
+        speed += 20;
+        Console.WriteLine($"Автомобиль {avto_number} ускоряется вжихххх. Скорость : {speed}");
+    }
+
+    public void Tormozhenie()
+    {
+        speed = 0;
+        Console.WriteLine($"Автомобиль {avto_number} тормозит. Скорость : {speed} км/ч");
+    }
+
+    private int RaschetRasstoyaniya(int x1, int y1, int x2, int y2)
+    {
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+
+        double distance = Math.Sqrt(dx * dx + dy * dy);
+
+        return (int)Math.Round(distance);
+    }
+    public string GetNumber()
+    {
+        return avto_number;
+    }
+
+    public static void Avariya()
+    {
+        if (carCount == 0)
+        {
+            Console.WriteLine("Нет машин для аварии.");
+            return;
+        }
+        Random rnd = new Random();
+        Console.WriteLine("Произошли аварии с машинами : ");
+
+        int skokoAvariy = rnd.Next(1, Math.Min(4, carCount + 1));
+
+        for (int i = 0; i < skokoAvariy; i++)
+        {
+            int index = rnd.Next(0, carCount);
+            Console.WriteLine($"Машина {index + 1} ({allCars[index].GetNumber()}) попала в аварию!!!");
+            allCars[index].Ostatok();
+        }
+    }
+
+    private int Ostatok()
+    {
+        Console.WriteLine("Автомобиль остановился");
+        return (int)petrol;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,136 @@
+﻿
+class Program
+{
+    public static int ReadInt()
+    {
+        while (true)
+        {
+            if (int.TryParse(Console.ReadLine(), out int result) && result > 0)
+                return result;
+            Console.Write("Нельзя вводить отрицательные числа и буквы. Введите положительное число: ");
+        }
+    }
+
+    public static float ReadFloat()
+    {
+        while (true)
+        {
+            if (float.TryParse(Console.ReadLine(), out float result) && result > 0)
+                return result;
+            Console.Write("Нельзя вводить отрицательные числа и буквы. Введите положительное число: ");
+        }
+    }
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Меню:");
+            Console.WriteLine("1 - Добавить машину");
+            Console.WriteLine("2 - Показать информацию о всех машинах");
+            Console.WriteLine("3 - Совершить поездку");
+            Console.WriteLine("4 - Заправить машину");
+            Console.WriteLine("5 - Авария");
+            Console.WriteLine("6 - Выход");
+            Console.WriteLine();
+            Console.Write("Выберите действие: ");
+            if (!int.TryParse(Console.ReadLine(), out int menu) && menu < 1 || menu > 6)
+            {
+                Console.WriteLine("Введите число от 1 до 6.");
+                continue;
+            }
+
+            switch (menu)
+            {
+                case 1:
+                    if (Avto.carCount >= Avto.allCars.Length)
+                    {
+                        Console.WriteLine("Места для новых машин нет");
+                        break;
+                    }
+
+                    Console.Write("Введите номер новой машины: ");
+                    string num = Console.ReadLine();
+                    Console.Write("Обьем бака: ");
+                    float tank = ReadFloat();
+
+                    float bak;
+                    while (true)
+                    {
+                        Console.Write("Заправте бак (литры): ");
+                        bak = ReadFloat();
+                        if (bak <= tank)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Нельзя заправить больше чем есть в баке");
+                        }
+                    }
+                    Console.Write("Введите расход (л/100км): ");
+                    float ras = ReadFloat();
+                    Avto newAvto = new Avto(num, bak, ras, tank);
+                    Avto.allCars[Avto.carCount++] = newAvto;
+                    Console.WriteLine("Машина успешно добавлена");
+                    break;
+
+                case 2:
+                    if (Avto.carCount == 0)
+                    {
+                        Console.WriteLine("Нет доступных машин для поездки");
+                        break;
+                    }
+                    Console.WriteLine("-------------------------------------------------");
+                    Console.WriteLine("Информация о машинах:");
+                    Console.WriteLine("-------------------------------------------------");
+                    for (int i = 0; i < Avto.carCount; i++)
+                    {
+                        Console.WriteLine($"Машина {i + 1}:");
+                        Avto.allCars[i].Out();
+                        Console.WriteLine();
+                    }
+                    break;
+
+                case 3:
+                    Console.WriteLine("-------------------------------------------------");
+                    Console.WriteLine("Выберите номер машины для поездки");
+                    int moveAvto = ReadInt() - 1;
+                    if (moveAvto >= 0 && moveAvto < Avto.carCount)
+                    {
+                        Avto.allCars[moveAvto].Move();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неверный номер машины.");
+                    }
+                    break;
+
+                case 4:
+                    Console.WriteLine("-------------------------------------------------");
+                    Console.WriteLine("Выберите номер машины для заправки");
+                    int zapravkaAvto = ReadInt() - 1;
+                    if (zapravkaAvto >= 0 && zapravkaAvto < Avto.carCount)
+                    {
+                        Console.Write("Сколько литров заправить: ");
+                        float top = ReadFloat();
+                        Avto.allCars[zapravkaAvto].Zapravka(top);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неверный номер машины.");
+                    }
+                    break;
+
+                case 5:
+                    Console.WriteLine("-------------------------------------------------");
+                    Avto.Avariya();
+                    break;
+
+                case 6:
+                    Console.WriteLine("Выход из программы");
+                    return;
+            }
+
+        }
+    }
+}
